f9b2425cd1b9fdb79c4005db5de8248b74270f37
Frederick Agbo <agbofrederick230@gmail.com>
2024-09-16 16:10:52 +0000

    remove light/dark theme selector

--- src/layouts/headers/DefaultHeader.js
+++ src/layouts/headers/DefaultHeader.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import appData from "@data/app.json";
-import { useLocalStorage } from "@common/useLocalStorage";
+// import { useLocalStorage } from "@common/useLocalStorage";
 
 const DefaultHeader = ({ contactButton, cartButton }) => {
   const navItems = [];
@@ -16,7 +16,7 @@ const DefaultHeader = ({ contactButton, cartButton }) => {
     navItems.push(newobj);
   });
 
-  const [themeUI, setThemeUIToggle] = useLocalStorage('theme_ui', '');
+  // const [themeUI, setThemeUIToggle] = useLocalStorage('theme_ui', '');
   const [desktopMenu, desktopMenuToggle] = useState(false);
   const [mobileMenu, mobileMenuToggle] = useState(false);
   const [cartOpen, setCartOpen] = useState(false);
@@ -42,30 +42,30 @@ const DefaultHeader = ({ contactButton, cartButton }) => {
     e.target.parentNode.classList.toggle('active');
   }
 
-  const clickedThemeUI = () => {
-    setThemeUIToggle(!themeUI);
+  // const clickedThemeUI = () => {
+  //   setThemeUIToggle(!themeUI);
     
-    const lightmodeToggle = document.querySelector('#theme-icon');
-    if ( themeUI ) {
-      document.body.classList.remove('light-d');
-      lightmodeToggle.src = '/images/sun.png';
-    } else {
-      document.body.classList.add('light-d');
-      lightmodeToggle.src = '/images/moon.png';
-    }
-  }
+  //   const lightmodeToggle = document.querySelector('#theme-icon');
+  //   if ( themeUI ) {
+  //     document.body.classList.remove('light-d');
+  //     lightmodeToggle.src = '/images/sun.png';
+  //   } else {
+  //     document.body.classList.add('light-d');
+  //     lightmodeToggle.src = '/images/moon.png';
+  //   }
+  // }
 
-  useEffect(() => {
-    const lightmodeToggle = document.querySelector('#theme-icon');
+  // useEffect(() => {
+  //   const lightmodeToggle = document.querySelector('#theme-icon');
 
-    if ( themeUI ) {
-      document.body.classList.add('light-d');
-      lightmodeToggle.src = '/images/moon.png';
-    } else {
-      document.body.classList.remove('light-d');
-      lightmodeToggle.src = '/images/sun.png';
-    }
-  }, []);
+  //   if ( themeUI ) {
+  //     document.body.classList.add('light-d');
+  //     lightmodeToggle.src = '/images/moon.png';
+  //   } else {
+  //     document.body.classList.remove('light-d');
+  //     lightmodeToggle.src = '/images/sun.png';
+  //   }
+  // }, []);
 
   return (
     <header className="header-style-one" >
@@ -109,18 +109,18 @@ const DefaultHeader = ({ contactButton, cartButton }) => {
                       </ul>
                       
                       <div className="extras">
-                        <div className="theme-color">
+                        {/* <div className="theme-color">
                           <img 
                             src={ 1 ? "/images/moon.png" : "/images/sun.png"} 
                             alt="theme color" 
                             id="theme-icon" 
                             onClick={() => clickedThemeUI() } 
                           />
-                        </div>
+                        </div> */}
                         <a href="#" id="mobile-menu" className={mobileMenu ? "menu-start open" : "menu-start"} onClick={ (e) => clickedMobileMenu(e) }>
                           <svg id="ham-menu" viewBox="0 0 100 100"> <path className="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" /> <path className="line line2" d="M 20,50 H 80" /> <path className="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" /> </svg>
                         </a>
-                        <a href="#" id="desktop-menu" className={desktopMenu ? "menu-start open" : "menu-start"} onClick={ (e) => clickedDesktopMenu(e) }>
+                        <a href="#" id="desktop-menu" className={desktopMenu ? "menu-start open pl-2" : "menu-start pl-2"} onClick={ (e) => clickedDesktopMenu(e) }>
                           <svg id="ham-menue" viewBox="0 0 100 100"> <path className="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" /> <path className="line line2" d="M 20,50 H 80" /> <path className="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" /> </svg>
                         </a>
                         {cartButton == 1 &&
