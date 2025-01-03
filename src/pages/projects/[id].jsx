d567de90e61ed618dbcc92689dbbf8db96454fec
Gaius-1 <agbofrederick230@gmail.com>
2025-01-03 23:43:21 +0000

    improve ui of the project details page

--- src/pages/projects/[id].jsx
+++ src/pages/projects/[id].jsx
@@ -37,22 +37,6 @@ const ProjectDetail = ( props ) => {
       <section className="gap detail-page">
 
         <div className="container">
-          <ul className="next-prev-projects">
-            {prev_id != 0 && prev_id != undefined &&
-            <li className="prev">
-              <Link href={`/projects/${prev_id}`}>
-                <i className='fa-solid fa-arrow-left-long' />
-              </Link>
-            </li>
-            }
-            {next_id != 0 && next_id != undefined &&
-            <li className="next">
-              <Link href={`/projects/${next_id}`}>
-                <i className='fa-solid fa-arrow-right-long' />
-              </Link>
-            </li>
-            }
-          </ul>
           <div className="row">
             <div className="col-lg-8 offset-lg-2">
               <div className="pr-dat">
@@ -106,30 +90,6 @@ const ProjectDetail = ( props ) => {
           </div>
         </div>
       </section>
-      
-      {typeof postData.slider != "undefined" &&
-        <>
-          {/* Project Detail Slider Start */}
-          <div className="gap no-top project-detail-slider">
-            <div className="container-fluid g-0">
-              <Swiper
-                {...sliderProps.projDetailsSlider}
-                className="p-d-slider"
-              >
-                {postData.slider.items.map((item, key) => (
-                <SwiperSlide key={`pds-slide-${key}`} className="swiper-slide">
-                <figure>
-                  <img src={item.image} alt={item.alt} />
-                </figure>
-                </SwiperSlide>
-                ))}
-                <div className="swiper-pagination" />
-              </Swiper>
-            </div>
-          </div>
-          {/* Project Detail Slider Start */}
-        </>
-      }
 
       <RelatedProjectsSection projects={props.related} />
 
